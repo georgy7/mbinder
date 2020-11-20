243398d4c0c7868ba72882ddb700321e82f60a47
Георгий У <httpcatharsis@gmail.com>
2020-11-20 22:09:16 +0300

    Refactoring: extracted check_part, save (a part).

--- extract_mbox_attachments.py
+++ extract_mbox_attachments.py
@@ -7,17 +7,20 @@
 # Attachments will be extracted into subfolder "attachments" 
 # with prefix "n " where "n" is an order of attachment in mbox file. 
 
-import mailbox, pickle, traceback, os
+import mailbox
+import os, errno
+import pathlib      # since Python 3.4
+import traceback
 from email.header import decode_header
 
 prefs = {
     'file': 'all.mbox',
     'save_to': 'attachments/',
     'start': 0,
-    'stop': 100000000000   # On which message to stop (not included).
+    'stop': 100000000000  # On which message to stop (not included).
 }
 
-assert(os.path.isfile(prefs['file']))
+assert (os.path.isfile(prefs['file']))
 
 mb = mailbox.mbox(prefs['file'])
 
@@ -27,38 +30,62 @@ if not os.path.exists(prefs['save_to']):
 total = 0
 failed = 0
 
-def save_attachments(mid):
+
+def save(mid, part):
+    global total
+    total = total + 1
+
+    try:
+        decoded_name = decode_header(part.get_filename())
+
+        if isinstance(decoded_name[0][0], str):
+            name = decoded_name[0][0]
+        else:
+            name_encoding = decoded_name[0][1]
+            name = decoded_name[0][0].decode(name_encoding)
+
+        name = '%s %s' % (mid, name)
+
+        try:
+            with open(prefs['save_to'] + name, 'wb') as f:
+                f.write(part.get_payload(decode=True))
+        except OSError as e:
+            if e.errno == errno.ENAMETOOLONG:
+
+                extension = pathlib.Path(name).suffix
+                extension = extension if len(extension) <= 20 else ''
+
+                short_name = str(mid) + extension
+
+                with open(prefs['save_to'] + short_name, 'wb') as f:
+                    f.write(part.get_payload(decode=True))
+            else:
+                raise
+
+    except:
+        traceback.print_exc()
+        global failed
+        failed = failed + 1
+
+
+def check_part(mid, part):
+    if part.get_content_disposition() == 'attachment':
+        save(mid, part)
+    elif part.is_multipart():
+        for p in part.get_payload():
+            check_part(mid, p)
+
+
+def process_message(mid):
     msg = mb.get_message(mid)
     if msg.is_multipart():
         for part in msg.get_payload():
-            if part.get_content_disposition() != 'attachment':
-                continue
-
-            global total
-            total = total + 1
-
-            try:
-                decoded_name = decode_header(part.get_filename())
-
-                if isinstance(decoded_name[0][0], str):
-                    name = decoded_name[0][0]
-                else:
-                    name_encoding = decoded_name[0][1]
-                    name = decoded_name[0][0].decode(name_encoding)
-
-                name = '%s %s' % (mid, name)
-
-                with open(prefs['save_to'] + name, 'wb') as f:
-                    f.write(part.get_payload(decode=True))
-            except:
-                traceback.print_exc()
-                global failed
-                failed = failed + 1
+            check_part(mid, part)
 
 
 for i in range(prefs['start'], prefs['stop']):
     try:
-        save_attachments(i)
+        process_message(i)
     except KeyError:
         print('The whole mbox file was processed.')
         break
